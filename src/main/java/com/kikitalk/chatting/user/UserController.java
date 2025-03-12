b8ddfdf9dd0a111604ba74305f21d0e1eea51bbb
gogoyubae <uunnuibae@gmail.com>
2025-03-12 13:52:05 +0900

    :art: Refactor      : @AuthenticationPrincipal에서 @RequestHeader로 변경

--- src/main/java/com/kikitalk/chatting/user/UserController.java
+++ src/main/java/com/kikitalk/chatting/user/UserController.java
@@ -1,6 +1,7 @@
 package com.kikitalk.chatting.user;
 
 
+import com.kikitalk.chatting.security.jwt.JwtProvider;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,23 +21,22 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private JwtProvider jwtProvider;
 
     // 전화번호로 전체 유저 검색
     @GetMapping("/search")
     public ResponseEntity<User> searchFriend(@AuthenticationPrincipal User user, String phone) {
         return ResponseEntity.ok(userService.getUserByPhone(phone));
     }
-    // 유저 프로필
+
     @GetMapping("/profile")
-    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal User authenticatedUser) {
-        Long id = authenticatedUser.getId();
-        log.info("auto id -> {}", id);
+    public ResponseEntity<?> getUserProfile(@RequestHeader("Authorization") String token) {
+        log.info("String -> {}", token);
+        String accessToken = token.replace("Bearer ", "");
+        Long id = Long.parseLong(jwtProvider.getId(accessToken));
         Optional<User> user = userService.getUser(id);
-        if (user.isPresent()) {
-            return ResponseEntity.ok().body(user.get());
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-        }
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
     // 유저 이름 찾기
     @GetMapping("/username/{id}")
